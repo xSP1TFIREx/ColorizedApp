00a437853127d758d9b6a1db16af422280c7dd18
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-10-28 22:45:42 +0300

    Initial commit

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  ColorizedApp
-//
-//  Created by Дмитрий on 28.10.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bigView: UIView!
+    @IBOutlet weak var redLabelValue: UILabel!
+    @IBOutlet weak var greenLabelValue: UILabel!
+    @IBOutlet weak var blueLabelValue: UILabel!
+    @IBOutlet weak var redSliderOutlet: UISlider!
+    @IBOutlet weak var greenSliderOutlet: UISlider!
+    @IBOutlet weak var blueSliderOutlet: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func redSliderAction() {
+        print(redSliderOutlet.value)
+        
+    }
+   
+    @IBAction func greenSliderAction() {
+        print(greenSliderOutlet.value)
+    }
+    
+    
+    @IBAction func blueSliderAction() {
+        print(blueSliderOutlet.value)
+    }
+    
 }
 
