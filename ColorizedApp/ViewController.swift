582ffd33c452c7abe8ff73f2f585661f4dc39a8e
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-10-28 23:11:21 +0300

    Ready project

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -13,21 +13,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        bigViewInitial()
+        changeColor()
+        setupValue()
+        
     }
 
     @IBAction func redSliderAction() {
-        print(redSliderOutlet.value)
-        
+        changeColor()
+        setupValue()
     }
    
     @IBAction func greenSliderAction() {
-        print(greenSliderOutlet.value)
+        changeColor()
+        setupValue()
     }
-    
     
     @IBAction func blueSliderAction() {
-        print(blueSliderOutlet.value)
+        changeColor()
+        setupValue()
     }
     
+//    MARK: - Initial bigView
+    private func bigViewInitial() {
+        bigView.layer.cornerRadius = 30
+    }
+    
+//    MARK: - Change Background color
+    private func changeColor() {
+        bigView.backgroundColor = UIColor(
+            red: CGFloat(redSliderOutlet.value),
+            green: CGFloat(greenSliderOutlet.value),
+            blue: CGFloat(blueSliderOutlet.value),
+            alpha: 1
+        )
+    }
+    
+//    MARK: - Setup Slider`s Value
+    private func setupValue() {
+        redLabelValue.text = String(format: "%.2f", redSliderOutlet.value)
+        greenLabelValue.text = String(format: "%.2f", greenSliderOutlet.value)
+        blueLabelValue.text = String(format: "%.2f", blueSliderOutlet.value)
+    }
 }
+
 
